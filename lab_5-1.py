876c5d6d02e3475ba193787784c5ad467691ac93
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-10-04 09:56:19 -0400

    Submit Lab 5-1

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# Author: CRS 10/03/21
+
+# int(a)
+# Name error.
+
+# int('a')
+# Value error.
+
+# 'a' + 2
+# Type error.
+
+# import date
+# Module not found error.
+
+# print("I'm a happy camper!)
+# Syntax error.
